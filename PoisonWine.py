f82a939c637b4be32700dfea84fed5993dce502d
Frode Haugsand <PainSled@users.noreply.github.com>
2017-01-24 21:19:50 +0100

    Poison Wine Challenge
    https://www.youtube.com/watch?v=N3qmN6pYhi0

---
+++ PoisonWine.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+
+import math
+import random
+
+nWine = 1000
+nMice = math.ceil(math.log(nWine, 2))
+
+badWine = random.randrange(1, nWine, 1)
+
+def testWineBottles(wineBottles):
+    for n in wineBottles:
+        if n == badWine:
+            return True
+    return False
+
+def selectRange(mouseNumber):
+    rng = []
+    n = 0
+    while n <= nWine:
+        for r in range(0,2**mouseNumber):
+            n += 1
+        for r in range(0,2**mouseNumber):
+            n += 1
+            rng.append(n)
+    return rng
+
+
+def main():
+    print("Number of wine bottles: " + str(nWine))
+    print("Number of experimental mice: " + str(nMice) + "\n")
+    
+    estimate = 1 # We started at 1, not 0, when numbering the wine.
+    tally = []
+    for i in range(0, nMice):
+        dead = testWineBottles(selectRange(i))
+        tally.append([2**i, dead])
+        if dead:
+            estimate += 2**i 
+
+    for number, status in tally:
+        print(str(number) + "\t", end="")
+    print("")
+    for number, status in tally:
+        print(("Dead" if status else "Alive") + "\t", end="")
+    print("\n\nBinary: ", end="")
+    for number, status in tally:
+        print(("1" if status else "0"), end="")
+
+    print("\n\nEstimated poisoned wine: " + str(estimate))
+    print("Specific poisoned wine: " + str(badWine) + "\n")
+
+main()
