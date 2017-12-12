a685c2cbadac6e46bb966012762084b5c043e5b7
Frode Haugsand <PainSled@users.noreply.github.com>
2017-12-12 14:46:40 +0100

    Update testsort.c
    First working example?

--- testsort.c
+++ testsort.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
 
-#define dataType unsigned char
+#define listDType unsigned short
+#define sortDType unsigned char
 
 int main()
 {
-    dataType list[4] = {4, 6, 8, 10};
-    dataType newNumber = 5;
+    listDType list[4] = {4, 6, 8, 10};
+    listDType newNumber = 9;
     
     
-    printf("New number: %i\nOld list: ", newNumber);
+    printf("New number: %d\nOld list: ", newNumber);
     
-    unsigned short nVarInList = sizeof(list)/sizeof(list[0]);
+    sortDType nVarInList = sizeof(list)/sizeof(list[0]);
     
-    for ( unsigned char i = 0; i < nVarInList; i++ ){
-        printf("%i, ", list[i]);
+    for ( sortDType i = 0; i < nVarInList; i++ ){
+        printf("%d, ", list[i]);
     }
     
+    // If newNumber is less than median, start at beginning and cascade towards end.
     if ( newNumber < list[sizeof(list)/sizeof(list[0])/2] ){
+        printf("\nBob's your uncle");
+        for ( sortDType i = 0; i < nVarInList; i++ ){
+            printf("\nIs %d greater than %d? ", list[i], newNumber);
+            if ( list[i] > newNumber ){
+                printf("YES!", i);
+                //memmove(&items[k+1], &items[k], (numItems-k-1)*sizeof(double)); //LOOK UP THIS FOR POROBABLE INCREASE IN EFFICIENCY
+                
+                //Insert newNumber, and shift list towards end.
+                for ( sortDType n = nVarInList; n > i; n-- ){
+                    list[n] = list[n-1];
+                }
+                list[i] = newNumber;
+                
+                break;
+            } else{
+                printf("NO!", i);
+            }
+        }
+    }
+    // Else, start at end and cascade towards beginning.
+    else {
+        printf("\nBob's your auntie");
+        for ( sortDType i = nVarInList-1; i > 0; i-- ){
+            printf("\nIs %d less than %d? ", list[i], newNumber);
+            if ( list[i] < newNumber ){
+                printf("YES!", i);
+                //memmove(&items[k+1], &items[k], (numItems-k-1)*sizeof(double)); //LOOK UP THIS FOR POROBABLE INCREASE IN EFFICIENCY
+                
+                //Insert newNumber, and shift list towards end.
+                for ( sortDType n = 0; n < i; n++ ){
+                    list[n] = list[n+1];
+                }
+                list[i] = newNumber;
+                
+                break;
+            } else{
+                printf("NO!", i);
+            }
+        }
         
     }
     
@@ -25,13 +66,13 @@ int main()
     
     nVarInList = sizeof(list)/sizeof(list[0]);
     
-    for ( unsigned char i = 0; i < nVarInList; i++ ){
-        printf("%i, ", list[i]);
+    for ( sortDType i = 0; i < nVarInList; i++ ){
+        printf("%d, ", list[i]);
     }
     
-    printf("\n\n%i\n", sizeof(list[0]));
-    printf("%i\n", sizeof(list));
-    printf("%i\n", nVarInList);
+    printf("\n\n%d\n", sizeof(list[0]));
+    printf("%d\n", sizeof(list));
+    printf("%d\n", nVarInList);
 
     return 0;
 }
