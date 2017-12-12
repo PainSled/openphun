4383f167d00932a40c3d66cab1fc7ac01aeff42d
Frode Haugsand <PainSled@users.noreply.github.com>
2017-12-12 14:04:20 +0100

    Update testsort.c

--- testsort.c
+++ testsort.c
@@ -5,15 +5,33 @@
 int main()
 {
     dataType list[4] = {4, 6, 8, 10};
-    dataType newNumber = 8;
+    dataType newNumber = 5;
     
-    if ( newNumber < list[sizeof(list)/sizeof(list[0])/2] ){
-        printf("Bob's your auntie\n");
+    
+    printf("New number: %i\nOld list: ", newNumber);
+    
+    unsigned short nVarInList = sizeof(list)/sizeof(list[0]);
+    
+    for ( unsigned char i = 0; i < nVarInList; i++ ){
+        printf("%i, ", list[i]);
     }
     
-    printf("%i\n", sizeof(list[0]));
+    if ( newNumber < list[sizeof(list)/sizeof(list[0])/2] ){
+        
+    }
+    
+    
+    printf("\nNew list: ");
+    
+    nVarInList = sizeof(list)/sizeof(list[0]);
+    
+    for ( unsigned char i = 0; i < nVarInList; i++ ){
+        printf("%i, ", list[i]);
+    }
+    
+    printf("\n\n%i\n", sizeof(list[0]));
     printf("%i\n", sizeof(list));
-    printf("%i\n", sizeof(list)/sizeof(list[0]));
+    printf("%i\n", nVarInList);
 
     return 0;
 }
