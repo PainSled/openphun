631f2f5db7bd662569e9ef1b6fd4276e94b425d1
Frode Haugsand <PainSled@users.noreply.github.com>
2017-12-14 15:42:25 +0100

    Update testsort.c
    Cleanup, and more precise benchmarking

--- testsort.c
+++ testsort.c
@@ -5,9 +5,8 @@
 #define listDType unsigned short
 #define sortDType unsigned char
 
-#define listSize 8
+#define listSize 32
 
-sortDType nElmInList;
 listDType list[listSize];
 
 unsigned long getMicrotime(){
@@ -69,61 +68,41 @@ int main()
     unsigned int startTime;
     
     listDType input[listSize*2];
-    
-    
     for ( sortDType i = 0; i < listSize; i++ ){
         list[i] = 0;
     }
     
     srand((unsigned int)getMicrotime());
-    
     for ( sortDType i = 0; i < listSize*2; i++ ){
         input[i] = 10 + rand() % 89;
     }
     
-    nElmInList = sizeof(list)/sizeof(list[0]);
-    printf("\n\nnElmInList: %d\n\n", nElmInList);
+    for ( sortDType i = 0; i < listSize*2; i++ ){
+        rollOver( input[i] );
+        if ( 0 ){
+        printf("\nNew number: %d List: ", input[i]);
+            for ( sortDType i = 0; i < listSize; i++ ){
+                printf("%d, ", list[i]);
+            }
+        }
+    
+    }
     
     printf("\nOld list: ");
     for ( sortDType i = 0; i < listSize; i++ ){
         printf("%d, ", list[i]);
     }
-    printf("\n\nnElmInList: %d\n\n", nElmInList);
-    
-    printf("\n\nSize of list element: %d\n", sizeof(list[0]));
-    printf(    "Size of list:         %d\n", sizeof(list));
-    printf(    "Elements in list:     %d\n", nElmInList);
-    
-    printf("\n\nnElmInList: %d\n\n", nElmInList);
-    
-    
     
     startTime = getMicrotime();
-    
-    for ( sortDType i = 0; i < listSize*2; i++ ){
+    for ( sortDType i = 0; i < listSize; i++ ){
         rollOver( input[i] );
-        if ( 1 ){
-        printf("\nNew number: %d List: ", input[i]);
-            for ( sortDType i = 0; i < listSize; i++ ){
-                printf("%d, ", list[i]);
-        }
-    }
-    
     }
     endTime = getMicrotime();
-    
-    printf("\n\nnElmInList: %d\n", nElmInList);
-    nElmInList = sizeof(list)/sizeof(list[0]);
-    printf("\nnElmInList: %d\n", nElmInList);
     
     printf("\nNew list: ");
     for ( sortDType i = 0; i < listSize; i++ ){
         printf("%d, ", list[i]);
     }
-    
-    printf("\n\nSize of list element: %d\n", sizeof(list[0]));
-    printf(    "Size of list:         %d\n", sizeof(list));
-    printf(    "Elements in list:     %d\n", nElmInList);
     
     printf("\n\nMicroseconds elapsed: %d\n", endTime-startTime);
 
